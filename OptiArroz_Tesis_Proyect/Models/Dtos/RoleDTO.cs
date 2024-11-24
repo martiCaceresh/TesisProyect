5cd49ecba8835a23d19f76a658f2d24f504d7f61
martiCaceresh <82267708+martiCaceresh@users.noreply.github.com>
2024-11-23 19:41:36 -0500

    role

--- OptiArroz_Tesis_Proyect/Models/Dtos/RoleDTO.cs
+++ OptiArroz_Tesis_Proyect/Models/Dtos/RoleDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace OptiArroz_Tesis_Proyect.Models.Dtos
 {
-    public class RolDTO
+    public class RoleDTO
     {
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;

830c9538c975acea762e3013328e01580ee96459
martiCaceresh <jcaceresh@pucp.edu.pe>
2024-12-01 18:12:12 -0500

    RiceLotMovement Mobile
    https://localhost:44344/mobile/RiceLotMovement/32

--- OptiArroz_Tesis_Proyect/Controllers/MobileController.cs
+++ OptiArroz_Tesis_Proyect/Controllers/MobileController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OptiArroz_Tesis_Proyect.Data.DataAccess;
 using OptiArroz_Tesis_Proyect.Data.Interfaces;
 using OptiArroz_Tesis_Proyect.Models.Dtos;
 using OptiArroz_Tesis_Proyect.Models.Entities;
@@ -11,17 +12,21 @@ namespace OptiArroz_Tesis_Proyect.Controllers
     public class MobileController : Controller
     {
         private readonly IRiceClassificationDA RiceClassificationDA;
+        private readonly IRiceLotMovementDA RiceLotMovementDA;
         private readonly IRiceLotDA RiceLotDA;
+        private readonly IZoneDA ZoneDA;
         private readonly IMapper Mapper;
         public readonly UserManager<ApplicationUser> UserManager;
         private readonly IConfiguration Configuration;
 
-        public MobileController(IConfiguration Configuration, IRiceLotDA RiceLotDA, IRiceClassificationDA RiceClassificationDA, IMapper Mapper, UserManager<ApplicationUser> UserManager)
+        public MobileController(IConfiguration Configuration, IRiceLotDA RiceLotDA, IRiceLotMovementDA RiceLotMovementDA, IZoneDA ZoneDA, IRiceClassificationDA RiceClassificationDA, IMapper Mapper, UserManager<ApplicationUser> UserManager)
         {
             this.UserManager = UserManager;
             this.Mapper = Mapper;
             this.RiceClassificationDA = RiceClassificationDA;
+            this.RiceLotMovementDA = RiceLotMovementDA;
             this.RiceLotDA = RiceLotDA;
+            this.ZoneDA = ZoneDA;
             this.Configuration = Configuration;
         }
 
@@ -36,6 +41,30 @@ namespace OptiArroz_Tesis_Proyect.Controllers
                 Model.Classifications = await RiceClassificationDA.GetActiveRiceClassifications();
                 var RiceLot = await RiceLotDA.GetRiceLotById(IdLot);
                 Model.RiceLotDetailDTO = Mapper.Map<RiceLotDetailDTO>(RiceLot);
+
+                ViewBag.IdLot = IdLot;
+
+                return View(Model);
+            }
+            else
+            {
+                return Unauthorized("Acceso restringido a dispositivos móviles.");
+            }
+        }
+
+        [Route("mobile/RiceLotMovement/{IdLot}")]
+        public async Task<IActionResult> RiceLotMovement(int IdLot)
+        {
+            var userAgent = Request.Headers["User-Agent"].ToString();
+
+            if (EsDispositivoMovil(userAgent))
+            {
+                var Model = new RiceLotMovementVM();
+                var RiceLot = await RiceLotDA.GetRiceLotById(IdLot);
+                Model.RiceLotDetailDTO = Mapper.Map<RiceLotDetailDTO>(RiceLot);
+                Model.Zones = await ZoneDA.GetActiveZones();
+                var RiceMovement = await RiceLotMovementDA.GetRiceLotMovementByLot(IdLot);
+                Model.RiceLotMovementTableDTO = Mapper.Map<List<RiceLotMovementTableDTO>>(RiceMovement);
 
                 ViewBag.IdLot = IdLot;
 
